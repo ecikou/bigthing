6f4497c22f7b4f460e09874951c844f40d0a7fd4
ecikou <747148841@qq.com>
2022-05-22 08:51:40 +0800

    提交了消息

--- assets/js/index.js
+++ assets/js/index.js
@@ -39,3 +39,29 @@ if (uer.user_pic !== null) {
     $(".text-avatar").html(firstName).show();
   }
 }
+
+// 退出登录
+$("#btnLogout").click(() => {
+    layui.layer.confirm(
+        "确定退出登录？",
+        { icon: 3, title: "" },
+        function (index) {
+            // 清空本地存储里面的 token
+            localStorage.removeItem("token");
+            // 重新跳转到登录页面
+            location.href = "/login.html";
+        }
+    );
+});
+
+// 不论成功还是失败，最终都会调用 complete 回调函数
+complete: (res) => {
+    // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+    if(res.responseJSON.status ===1 && res.responseJSON.message === "身份认证失败！") {
+        //  强制清空 token
+        localStorage.removeItem("token");
+        // 强制跳转到登录页面
+        location.href = "/login.html"
+    }
+}
+
